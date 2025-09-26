7f73fbc3b53f7ec5e61c0b59b10ee20fa7ec4d98
Abijith Raja <your-github-abijithraja0@gmail.com>
2025-09-26 10:03:17 +0530

    pratice

---
+++ Java/multiplicationtable.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class multiplicationtable{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of the table:");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            if (i==5 || i ==6){
+                continue;
+            }
+            else{
+            System.out.println(n+" * "+i+" = "+(n*i));
+            }
+        }
+    }
+}
