6169f504069facf368d8ad5e2b7226450af83136
abijithraja <abijithraja0@gmail.com>
2026-01-23 13:46:03 +0530

    pratice

--- Java/Solution.java
+++ Java/Solution.java
@@ -7,7 +7,7 @@ class Solution {
             }
             digits[i] = 0;
         }
-        int[] result = new int[digits.length + 1];
+         int[] result = new int[digits.length + 1];
         result[0] = 1 ; 
         
         return result;
