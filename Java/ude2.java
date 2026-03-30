bf1cd0beb3a4abbf2e6edb355a32913738e79c3f
abijithraja <abijithraja0@gmail.com>
2026-03-30 14:23:11 +0530

    Lernings

---
+++ Java/ude2.java
@@ -0,0 +1,12 @@
+public class ude2 {
+    public static void main(String args[]){
+        int n = 25 ;
+        for (int i =0 ; i<n ; i++){
+            System.out.println("The number is :"+i);
+            if (i==6){
+                System.out.println("Hello");
+                continue;
+            }
+        }
+    }
+}
