0ab65e1f36c9b86c9573bee60f7b6fd2a9973d5f
abijithraja <abijithraja0@gmail.com>
2026-07-17 14:24:36 +0530

    fix

--- Java/methodoverloading.java
+++ Java/methodoverloading.java
@@ -2,6 +2,7 @@ public class methodoverloading{
     public static void main (String args[]){
         calc(" ABI ",30);
         calc(40);
+        calc();
     }
     public static int calc(String playername , int score){
         System.out.println(" Player "  + playername + " score " + score);
@@ -11,6 +12,9 @@ public class methodoverloading{
         public static int calc(int score){
         System.out.println(" Player is unknown " + " score " + score);
         return score;
-
+    }
+    public static int calc(){
+        System.out.println(" Playername is unknown " );
+        return 0;
     }
 }
